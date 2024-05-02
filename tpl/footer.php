b98e47f64d2ec022ea78e5638debae3f7116ceae
X1Ro <120311383+X1Ro1@users.noreply.github.com>
2024-05-02 23:41:31 +1000

    Add files via upload

---
+++ tpl/footer.php
@@ -0,0 +1,14 @@
+    <div class="footer">
+        <div class="footer-container">
+            <div class="footer-inner">
+                <div class="footer-left"><h3>©Stud Clever 2024-2025</h3></div>
+                <div class="footer-center"><img src="../assets/img/logo/StudCleverBlack.svg" alt=""></div>
+                <div class="footer-right">
+                    <a href="../assets/ver/versionProject.php">Версии проекта</a>
+                    <a href="../forum/forum.php">Форумы</a>
+                    <a href="../conspect.php">Конспекты</a>
+                    <a href="../oNas.php">О Нас</a>
+                </div>
+            </div>
+        </div>
+    </div>
---
+++ tpl/header.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Главная</title>
+    <link rel="stylesheet" href="../assets/css/style.css">
+    <script defer src="../assets/js/burger.js"></script>
+</head>
+<body>
+    <div class="header">
+        <div class="header-container">
+            <div class="header-inner">
+
+                <button class="header-burger" id="burger">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </button>
+
+                <div class="header-left">
+                    <a href="../index.php"><img src="../assets/img/logo/StudCleverWhite.svg" alt=""></a>
+                </div>
+
+                <div class="header-center">
+                    <div class="header-menu">
+                        <a href="../forum/forum.php">Форумы</a>
+                        <a href="../conspect.php">Конспекты</a>
+                        <a href="../homework.php">Домашние задания</a>
+                        <a href="../oNas.php">О Нас</a>
+                    </div>
+                </div>
+                <div class="header-right">
+                    <a href="../authentication/profile.php"><h4>egorov.aandr</h4></a>
+                    <img src="../assets/img/logo/avatar.svg" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+
+
